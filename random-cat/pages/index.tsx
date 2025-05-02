26f9d9ba44a2f6e696ffe63779915b56190e5189
musix55 <musix55.dev@gmail.com>
2025-05-02 13:14:43 +0900

    first commit

---
+++ random-cat/pages/index.tsx
@@ -0,0 +1,75 @@
+import { GetServerSideProps, NextPage } from "next";
+import { useState } from "react";
+import styles from "./index.module.css";
+
+type Props = {
+    initialImageUrl: string;
+}
+
+const IndexPage: NextPage<Props> = ({ initialImageUrl }) => {
+    const [imageurl, setImageUrl] = useState(initialImageUrl);
+    const [loading, setLoading] = useState(false);
+
+    /*useEffect(() => {
+        fetchImage().then((newImage) => {
+            setImageUrl(newImage.url);
+            setLoading(false);
+        });
+    }, []);*/
+
+    const handleClick = async () => {
+        setLoading(true);
+        const newImage = await fetchImage();
+        setImageUrl(newImage.url);
+        setLoading(false);
+    }
+
+    return (
+        <div className={styles.page}>
+            <button onClick={handleClick}>他のにゃんこも見る</button>
+            <div className={styles.frame}>{loading || <img src={imageurl} />}</div>
+        </div>
+    );
+};
+export default IndexPage;
+
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+    const image = await fetchImage();
+    return {
+        props: {
+            initialImageUrl: image.url,
+        },
+    };
+};
+
+type Image = {
+    url: string;
+};
+
+const fetchImage = async (): Promise<Image> => {
+    const res = await fetch("https://api.thecatapi.com/v1/images/search");
+    const images: unknown = await res.json();
+    
+    try {
+        if (!Array.isArray(images)){
+            throw new Error("猫の画像が取得できませんでした");
+        }
+        const image: unknown = images[0];
+
+        if (!isImage(image)){
+            throw new Error("猫の画像が取得できませんでした");
+        }
+        return image;
+    }
+    catch {
+        throw new Error("猫の画像が取得できませんでした");
+    }
+};
+
+const isImage = (value: unknown): value is Image => {
+    if (!value || typeof value !== "object"){
+        return false;
+    }
+
+    return "url" in value && typeof value.url === "string";
+};
